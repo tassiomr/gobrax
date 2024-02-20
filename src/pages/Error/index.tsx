0cb2cd7be1788d92b36992364759bec4d569d837
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-20 09:54:21 -0300

    feat: add error page on routering

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import Container from "../../components/Container";
+
+export default function ErrorPage() {
+  return <Container>
+    <Typography>Error, page don`t exits</Typography>
+  </Container>
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import ErrorPage from "./pages/Error";
 
 export default createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />
   }
 ]);
