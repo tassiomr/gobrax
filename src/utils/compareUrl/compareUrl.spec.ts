191005d223439436e4024841cb4dcf127e72d26f
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-20 12:13:30 -0300

    test: configuration of vitest suits

---
+++ src/utils/compareUrl/compareUrl.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+
+import compareUrl from "./";
+
+describe('Testing compare URL component', () => {
+  it('Should return true if the given route matches the path', () => {
+     const route = 'drivers';
+     const path = '/drivers';
+
+    expect(compareUrl(route, path)).toBeTruthy();
+  });
+
+  it('Should return false if the given route does not match the path', () => {
+    const route = 'drivers';
+    const path = '/cars';
+
+    expect(compareUrl(route, path)).toBeFalsy();
+  });
+});
