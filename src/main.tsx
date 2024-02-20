5915c48fd52f444aa09c075779731b999b9e1f89
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-20 09:49:15 -0300

    feat: config theme and routes

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
