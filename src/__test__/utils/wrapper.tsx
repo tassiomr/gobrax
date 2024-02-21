5f830d2fb871377e6050b58e5c7e52f81ac4b21c
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-21 11:21:44 -0300

    refactor: config ui test on project using vitest and testing library

---
+++ src/__test__/utils/wrapper.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import theme from '../../theme';
+import { ReactNode } from "react";
+
+export const WrapperTestUtils = ({ children }: { children: ReactNode }) => {
+  return <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  </BrowserRouter>
+}
--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   test: {
+    globals: true,
+    setupFiles: ['./vitest-setup.js'],
+    environment: 'jsdom',
     coverage: {
       enabled: true,
       provider: 'istanbul',
-      reporter: ['text', 'html', 'clover', 'json']
-    }
+    },
   }
 });
