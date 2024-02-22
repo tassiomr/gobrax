6dab3dcf407ecd6db2a5690b96339b9fd09c1b15
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-22 09:07:44 -0300

    styles: add uppercase in all cells

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -20,7 +20,7 @@ const theme = createTheme({
     MuiTableCell: {
       styleOverrides: {
         root: {
-          textTransform: 'capitalize',
+          textTransform: 'uppercase',
         },
       },
     },
